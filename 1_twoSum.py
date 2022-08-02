f2698163eff816e58fb55bd99b5b3b2e22b0cac7
alexhuynh1999 <50717720+alexhuynh1999@users.noreply.github.com>
2022-08-02 14:18:43 -0700

    Update 1_twoSum.py
    second soln

--- 1_twoSum.py
+++ 1_twoSum.py
@@ -10,3 +10,28 @@ class Solution:
                 
                 return [min(i, complements[target - n]), max(i, complements[target-n])]
         
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # brute force:
+        #   go through each elem in nums
+        #   sum that elem with every other elem individually
+        #   -> check if that equals target
+        #   
+        #   time:  O(n^2)
+        #   space: O(1)
+        
+        # complements soln:
+        #   go through each elem in nums
+        #   find the complement to the target (elem + complement = target)
+        #   store complements in a dictionary/set
+        #   go through each elem in nums again
+        #   check if elem is a complement in the set
+        #
+        #   time:  O(n)
+        #   space: O(n)
+        
+        complements = {}
+        for i, n in enumerate(nums):
+            if target - n in complements: return [i, complements[target - n]]
+            complements[n] = i
+        return False
