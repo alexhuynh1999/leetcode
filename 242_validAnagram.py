f1761e8d7ce64100ea4553098100c0f33f88d44d
alexhuynh1999 <50717720+alexhuynh1999@users.noreply.github.com>
2022-08-02 14:03:09 -0700

    Update 242_validAnagram.py
    second soln

--- 242_validAnagram.py
+++ 242_validAnagram.py
@@ -12,3 +12,27 @@ class Solution:
             freq_t[char] += 1
                 
         return freq_s == freq_t
+
+ class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # counter method: 
+        #   initialize a counter object of all the alphabet (a...z)
+        #   increment counter by 1 for each character seen
+        #   do this for s and t
+        #   check if counter objects are equivalent
+        #   
+        #   time:  O(max(s,t))
+        #   space: O(1) because only 26 characters. unicode would be the same; just a 255 char object
+        
+        if len(s) != len(t): return False
+        
+        alphabet = list(map(chr, range(97, 123)))
+        counterC = dict.fromkeys(alphabet, 0)
+        counterT = dict.fromkeys(alphabet, 0)
+        
+        for char in s:
+            counterC[char] += 1
+        for char in t:
+            counterT[char] += 1
+            
+        return counterC == counterT
