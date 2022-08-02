b0b7abfd25bf88f763c7052bde31f671c2ef749e
alexhuynh1999 <50717720+alexhuynh1999@users.noreply.github.com>
2022-08-02 13:53:01 -0700

    Update 217_containsDuplicate.py
    second soln

--- 217_containsDuplicate.py
+++ 217_containsDuplicate.py
@@ -5,3 +5,18 @@ class Solution:
             if n in seen: return True
             else: seen.add(n)
         return False
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # brute force:
+        #   touch each element in nums
+        #   check every element after that if it is equal
+        #   time:  o(n^2)
+        #   space: o(1)
+        
+        # faster:
+        #   make a set
+        #   check length of set == length of nums
+        
+        unique = set(nums)
+        return len(unique) != len(nums)
