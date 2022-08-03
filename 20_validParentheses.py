00a3603369bdd4419bb3ae94c1e6414b83f7be79
alexhuynh1999 <50717720+alexhuynh1999@users.noreply.github.com>
2022-08-03 16:37:54 -0700

    Update 20_validParentheses.py
    added space-time complexity

--- 20_validParentheses.py
+++ 20_validParentheses.py
@@ -5,6 +5,9 @@ class Solution:
         # if closed parenthesis is encountered, look at top of stack
         # if the top of stack is the corresponding closed parenthesis, continue
         # otherwise, return nothing
+        #
+        # time:  O(n) since we visit each eleme in array once
+        # space: O(n) since we store entire array on the stack
         closed = {')' : '(',
                   '}' : '{', 
                   ']' : '['}
