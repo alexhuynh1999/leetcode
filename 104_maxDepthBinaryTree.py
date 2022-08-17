89ad27fcde0f6b03be541c09fb94f96dc517ab59
alexhuynh1999 <50717720+alexhuynh1999@users.noreply.github.com>
2022-08-17 17:10:26 -0500

    Update 104_maxDepthBinaryTree.py
    second soln

--- 104_maxDepthBinaryTree.py
+++ 104_maxDepthBinaryTree.py
@@ -16,3 +16,31 @@ class Solution:
 
         depth = max(left, right, 1)
         return depth
+
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # have a depth counter
+        # 
+        # base case:
+        #   no children
+        #   return depth
+        #
+        # recursive case:
+        #   has children
+        #   return max of recursive call on left and right
+        if not root:
+            return 0
+        
+        if not (root.left or root.right):
+            return 1
+        
+        leftDepth = self.maxDepth(root.left) + 1
+        rightDepth = self.maxDepth(root.right) + 1
+        
+        return max(leftDepth, rightDepth)
