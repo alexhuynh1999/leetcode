c2fd501055d320db71c1af4c536ddb3882ab4141
alexhuynh1999 <50717720+alexhuynh1999@users.noreply.github.com>
2022-08-17 16:25:00 -0500

    Update 206_reverseLinkedList.py
    added second soln

--- 206_reverseLinkedList.py
+++ 206_reverseLinkedList.py
@@ -22,4 +22,37 @@ class Solution:
         head.next = prev
         
         return head
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # brute force:
+        #   iterate through the list
+        #   keep track of the previous node
+        #   set the current's next to the previous
+        # time:  O(n) 
+        # space: O(n) because we're basically copying the list
+        
+        # better space:
+        #   point the current node at the previous node
+        #   - initialize prev to none
+        #   - keep track of next (iter) because we will reassign current.next
+        
+        if not head:
+            return None
+        if not head.next:
+            return head
+        
+        prev = None
+        while head.next:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        head.next = prev
+        return head
         
